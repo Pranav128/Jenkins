ae008904317ce8f2ab69b5cea91cd692ee11e23c
Pranav128 <100985846+Pranav128@users.noreply.github.com>
2025-04-25 12:47:46 +0530

    Update build_mvn_dockerfile.groovy

--- vars/build_mvn_dockerfile.groovy
+++ vars/build_mvn_dockerfile.groovy
@@ -1,6 +1,6 @@
 def call(String repo, String tag){
   echo '🔨 Building all microservices using Maven and JIB...'
   bat 'mvn clean install -DskipTests'
-  bat 'docker build -t pranav128/${repo}:${tag} .'
+  bat "docker build -t pranav128/${repo}:${tag} ."
   echo "✅ Build successful"
 }
